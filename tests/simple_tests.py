0fd2945cdee0c9fd3533ad3604d8892de4a7e269
ifsheldon <39153080+ifsheldon@users.noreply.github.com>
2021-08-05 13:13:11 +0300

    add simple test to make sure runnable

---
+++ tests/simple_tests.py
@@ -0,0 +1,13 @@
+import mpl_lightning as mpl
+import pytorch_lightning as pl
+
+
+def test_data():
+    model = mpl.LightningMPL(10, 28, 2)
+    data_module = mpl.CIFAR10SSL_DM("./temp", 32, 32 * 4, 32, 4000, True, 1000, (2, 10), 1)
+    trainer = pl.Trainer(fast_dev_run=1, gpus=1)
+    trainer.fit(model, data_module)
+
+
+if __name__ == "__main__":
+    test_data()
